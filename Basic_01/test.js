8479d7e8bff2d0a885e6ff10c691e61fc6d06371
Sumit Lawand <116204178+Sumitlawand@users.noreply.github.com>
2023-11-17 21:38:10 +0000

    Container confic done

--- Basic_01/test.js
+++ Basic_01/test.js
@@ -1,2 +1,57 @@
 
-console.log("sumit");
+// E-02
+// run code on vs code not in console
+// console.log("Sumit");
+
+
+// E-03
+// Github account directly connected to vs code
+// go to github repo codespace 
+// then go online code editer vs code and than go to view -> command pallet (Ctrl+shift+P) -> go to container-> go to create envirment-> find node & javscript
+
+// E-04
+// const password = 1234567;   
+// let EmailID = "Sumitslawand@gmail.com"
+// var Score = "120"
+// acountCity = "Pune"
+
+// // password = 0849
+// console.log(password)
+// // password = 0849423;
+// EmailID = "Sumit@gmail.com";
+// var Score = "1000";
+// acountCity = "Satara"
+
+// console.table([password, EmailID, Score, acountCity])
+
+// in given example const value is never been change 
+// but let and var value is change
+// NOTE
+// TO declear all values in in bulk use console.table()
+
+// E-05
+// Data type in JS
+// there are 7 primative data types in javascript
+// 1.Symbol
+// 2.String
+// 3.Bolain
+// 4.Null
+// 5.undefine
+// 6.Bigint
+// 7.Number
+
+// console.log(typeof"Sumit")
+// console.log(typeof 18)
+// console.log(typeof true)
+// console.log(typeof null)
+// console.log(A)
+//  var A = "";
+
+
+
+// E-06
+
+
+
+
+
