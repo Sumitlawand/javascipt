2c144c3e570a757e756b912a185000fc19e1a278
Sumit Lawand <116204178+Sumitlawand@users.noreply.github.com>
2023-12-01 01:14:24 +0000

    EP- 30

--- Basic_01/test.js
+++ Basic_01/test.js
@@ -760,3 +760,44 @@ let  array =  ["Sumit", "Rahul" , "Raj","Sagar"]
 // data.forEach((item)=>{
 //     console.log(`${item.name}  ${item.lastname}`)
 // })
+
+// EP- 30
+
+// const data = [1,2,3,4,5,6,7,8,9,10]
+
+// data.forEach((item)=>{
+// return console.log(item)
+// })
+
+// const data = [1,2,3,4,5,6,7,8,9,10]
+
+// const datda = data.filter((num) => {
+//   return  num <= 5
+// })
+// console.log(datda)
+
+// const data = [1,2,3,4,5,6,7,8,9,10]
+
+
+// const dataA = [];
+
+// data.forEach((num)=>{
+// if (num < 4){
+//   dataA.push(num)
+// }
+// })
+
+// console.log(dataA)
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
