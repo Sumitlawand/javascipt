eb5a5a31b51c258d4d58d46b95c94093afba900d
Sumit Lawand <116204178+Sumitlawand@users.noreply.github.com>
2023-12-14 00:12:45 +0000

    p-4

---
+++ Projects/P4-Guess-The-Number.js
@@ -0,0 +1,24 @@
+const randomnumber = parseInt(Math.random() * 100 + 1)
+
+
+const Submit = document.querySelector('subt')
+const userInput = document.querySelector('guessField')
+// const guessSlot = document.querySelector('guess')
+const remaining = document.querySelector('lastResult')
+const guessSlot = document.querySelector('guesses')
+const lowOrHi = document.querySelector('lowOrHi')
+const startOver = document.querySelector('resultParas')
+
+
+const p = document.createElement('p')
+
+let prevguess = [];
+let numberofguess = 1;
+
+let playgame = true;
+
+function vadlidate (guess){
+    
+}
+
+
