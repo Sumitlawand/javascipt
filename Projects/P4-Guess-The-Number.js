ae2ac56709c87da841d51db8642a703344ec7b5f
Sumit Lawand <116204178+Sumitlawand@users.noreply.github.com>
2023-12-15 00:57:27 +0000

    P-4

--- Projects/P4-Guess-The-Number.js
+++ Projects/P4-Guess-The-Number.js
@@ -1,13 +1,14 @@
 const randomnumber = parseInt(Math.random() * 100 + 1)
 
 
-const Submit = document.querySelector('subt')
-const userInput = document.querySelector('guessField')
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
 // const guessSlot = document.querySelector('guess')
 const remaining = document.querySelector('lastResult')
 const guessSlot = document.querySelector('guesses')
 const lowOrHi = document.querySelector('lowOrHi')
 const startOver = document.querySelector('resultParas')
+
 
 
 const p = document.createElement('p')
@@ -17,8 +18,81 @@ let numberofguess = 1;
 
 let playgame = true;
 
-function vadlidate (guess){
-    
+if(playgame){
+  submit.addEventListener('click', (e)=>{
+  e.preventDefault();
+  const input  = parseInt(userInput.value)
+  vadlidateguess(input)
+  
+  })
 }
 
+function vadlidateguess (input){
+    if(isNaN(input)){
+      alert('Please enter valid number')
+    }else if (input > 100){
+      alert('Please enter valid number')
+    }else if (input < 1){
+      alert('Please enter valid number')
+    }else{
+      prevguess.push(input)
+      if(numberofguess === 11){
+        displayguess(input)
+        displaymassage(`End game random number was ${randomnumber}`)
+      }else{
+        checkguess(input)
+        displayguess(input)
+      }
+    }
+}
+
+function checkguess (input){
+  if(input === randomnumber){
+    displaymassage(`Congratulation you gusseing right number`)
+  }else if(input < randomnumber){
+    displaymassage(`gusseing low number`)
+  }
+  else if(input > randomnumber){
+    displaymassage(`gusseing high number`)
+  }
+}
+
+
+function displayguess (input){
+  userInput.value = '';
+  guessSlot.innerHTML += `${input}  `
+  numberofguess++;
+  remaining.innerHTML = `${11 - numberofguess}`
+
+}
+
+
+function displaymassage (massage){
+  lowOrHi.innerHTML = `<h2>${massage}</h2>`
+}
+
+function gameend (){
+   userInput.value = ''
+   userInput.setAttribute('disable', '')
+   p.classList('button')
+   p.innerHTML = `<h2 class="Submit">Start new Game<h2>`
+   startOver.appendChild(p)
+   playgame = false
+   gamestart()
+
+}
+
+function gamestart (){
+ const newgame = document.querySelector('Submit')
+ newgame.addEventListener('click', (e)=>{
+   userInput.removeAttribute('disable')
+   randomnumber = parseInt(Math.random() * 100 + 1)
+   prevguess = [];
+numberofguess = 1;
+remaining.innerHTML = `${11 - numberofguess}`
+startOver.removeChild(p)
+ 
+   playgame = true
+ })
+}
 
